663f69af6eec841f74b1ebdbaf4edaa71d6f88dc
Y-ME-tho <f20240969@pilani.bits-pilani.ac.in>
2025-07-22 23:01:53 +0530

    few templates

---
+++ Luv-CP-Course-master/Maths & Number Theory/Monk and Divisor Conundrum.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define lli long long int
+
+const int N = 2e5+10;
+
+int multiples_cnt[N];
+int hsh[N];
+
+
+int gcd(int a, int b){ // TC: O(log n) max
+	if(a == 0)
+		return b;
+	return gcd(b%a, a);
+}
+
+// // Question
+// Given an integer array A of size N, 
+// Monk needs you to answer T queries for him. 
+// In each query, he gives you 2 integers P and Q. 
+// In response to each of these queries, you need to tell Monk 
+// the count of numbers in array A. that are either divisible by P, Q, or both.
+
+// Link -> https://www.hackerearth.com/problem/algorithm/monk-and-divisor-conundrum-56e0eb99/
+
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+		int n, q; 
+		cin>>n;
+
+		// Storing array elements count
+		for(int i=0; i<n; i++){
+			int x;
+			cin>>x;
+			hsh[x]++;
+		}
+
+		cin>>q;
+
+		// Storing number of multiples count for each array element
+		for(int i=1; i<N; i++){
+			for(int j=i; j<N; j+=i){
+				multiples_cnt[i] += hsh[j];
+			}
+		}
+
+		// for(int i=0; i<=10; i++){
+		// 	cout<<i<<" "<<multiples_cnt[i]<<"\n";
+		// }
+
+		while(q--){
+			int p, q;
+			cin>>p>>q;
+
+			lli lcm = p*1LL*q/gcd(p,q);
+			lli ans = multiples_cnt[p]+multiples_cnt[q];
+			if(lcm < N)      // if lcm is out of range of given number range
+				ans -= multiples_cnt[lcm]; // 
+
+			cout<<ans<<"\n";
+		}
+
+	cout << "\n\n";
+	cout << (double)clock()/CLOCKS_PER_SEC << " sec" << endl;
+
+	return 0;
+}
